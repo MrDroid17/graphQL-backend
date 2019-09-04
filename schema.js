d0e32433cc02528f993d30f946626bbd460aaf99
Sobhit Kumar <sobhit2010@gmail.com>
2019-09-04 07:33:28 +0530

    importing graphql library

---
+++ schema.js
@@ -0,0 +1,13 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLNonNull
+} = require('graphql');
+
+module.exports = new GraphQLSchema({
+
+});

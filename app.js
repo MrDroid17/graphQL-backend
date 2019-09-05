98d0e1eb0338801016e23c0e36b320c8b53367f6
Sobhit Kumar <sobhit2010@gmail.com>
2019-09-05 07:25:46 +0530

    Add Schema

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
 const app = express();
+const schema = require('./schema.js');
 
 app.use('/graphql', expressGraphQL({
   schema:schema,
--- schema.js
+++ schema.js
@@ -8,6 +8,55 @@ const {
   GraphQLNonNull
 } = require('graphql');
 
-module.exports = new GraphQLSchema({
 
+const customers = [
+  {
+    id: '1', name: 'Sobhit Kumar', email: 'skumar@gmail.com', age: 28
+  },
+  {
+    id: '2', name: 'John doe', email: 'jhond@gmail.com', age: 35
+  },
+  {
+    id: '3', name: 'Warner bros', email: 'wbros@gmail.com', age: 34
+  },
+]
+
+// Customer type
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: {type: GraphQLString},
+    name: {type: GraphQLString},
+    email: {type: GraphQLString},
+    age: {type: GraphQLInt}
+  })
+})
+
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  field:{
+    customer: {
+      type: CustomerType,
+      args: {
+        id: {
+          type: GraphQLString
+        }
+      },
+      resolve(parentValue, args){
+        for(let i=0; i< customers.length; i++){
+          if(customers[i].id == args.id){
+            return customers[i];
+          }
+        }
+      }
+    },
+
+  }
+
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
